29383b10c28f4cd2caf1786478be4116be3a23bc
zakie071 <emmanuelprah51@gmail.com>
2026-01-02 16:42:14 +0000

    Day 2: User input and error handling

--- expenses.py
+++ expenses.py
@@ -1,17 +1,18 @@
 import json
 
-def save_expnses(expenses):
-    with open("data.json", "r") as file:
-        data = json.load(file)
-    data.append(expenses)
-    
-    with open("data.json", "w") as file:
-        json.dump(data, file, indent=4) 
        
 
 def add_expense(amount, category):
-    expenses={
+    return {
         "amount": amount,
         "category": category
     }
-    return expenses
+
+
+def save_expenses(expense):
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    data.append(expense)
+    
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4) 
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from expenses import add_expense, save_expnses
+from expenses import add_expense, save_expenses
 
-expense = add_expense(6000, "Education")
-save_expnses(expense)
-print(" Expense saved successfully!")
+
+
+def main():
+    try:
+        amount = float(input("Enter expense amount: "))
+        category = input("Enter expense category: ")
+
+        expense = add_expense(amount, category)
+        save_expenses(expense)
+        print(" Expenses saved successfully!")
+    
+    except ValueError:
+        print("Invalid input. Please enter a numeric value for amount.")
+    
+
+if __name__ == "__main__":
+    main()
